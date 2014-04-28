f77bf974900c3df636b2551777765a745ccecdba
raystation <rayuen@gmail.com>
2014-04-28 14:50:47 -0700

    add in sbfoundation

---
+++ sbfoundation/inc/footer.php
@@ -0,0 +1,7 @@
+    <script src="bower_components/jquery/dist/jquery.min.js"></script>
+    <script src="bower_components/foundation/js/foundation.min.js"></script>
+    <script src="js/app.js"></script>
+    <script src="slick/slick.min.js"></script>
+    <script src="js/slick.js"></script>
+  </body>
+</html>
---
+++ sbfoundation/js/slick.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+	$('.slideshow').slick({
+	  dots: true,
+	});
+});
